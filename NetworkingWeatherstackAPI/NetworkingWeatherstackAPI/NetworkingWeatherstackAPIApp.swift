f8f8ff81c4f0109f1b7bbc8d351de79550c6ebb4
hanisa1 <hanisamohamed1@gmail.com>
2025-03-28 18:58:01 +1100

    initial commit

---
+++ NetworkingWeatherstackAPI/NetworkingWeatherstackAPI/NetworkingWeatherstackAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingWeatherstackAPIApp.swift
+//  NetworkingWeatherstackAPI
+//
+//  Created by Hanisa Hilole on 28/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingWeatherstackAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
