93ba3094f3d643667a02df562e9359fdc50e72af
hanisa1 <hanisamohamed1@gmail.com>
2025-04-04 20:37:27 +1100

    Creating activity indicator to wait for API call to load

--- NetworkingWeatherstackAPI/NetworkingWeatherstackAPI/View/ViewController.swift
+++ NetworkingWeatherstackAPI/NetworkingWeatherstackAPI/View/ViewController.swift
@@ -11,11 +11,11 @@ class ViewController: UIViewController {
     
     private var weatherData: WeatherData?
     private var networkManager = NetworkManager.shared
+    private let spinnerView = SpinnerViewController()
     
     var textLabel: UILabel = {
         var label = UILabel()
         label.font = .systemFont(ofSize: 20, weight: .bold)
-        label.text = "hello"
         label.numberOfLines = 0
         label.textColor = .black
         return label
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
         
         Task {
             do {
-                weatherData = try await networkManager.getCityWeather(city: "New York")
+                weatherData = try await networkManager.getCityWeather(city: "Sydney")
                 DispatchQueue.main.async {
                     self.refreshView()
                 }
@@ -47,11 +47,12 @@ class ViewController: UIViewController {
     }
     
     private func setupView() {
-        
-        textLabel.text = "City: , Temperature, 0 , Sunrise:  "
+        createSpinnerView()
+//        textLabel.text = "City: , Temperature, 0 , Sunrise:  "
         view.backgroundColor = .systemBackground
         textLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(textLabel)
+        
         NSLayoutConstraint.activate([
             textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             textLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
@@ -60,10 +61,48 @@ class ViewController: UIViewController {
         
     }
     
+    func createSpinnerView() {
+
+        // add the spinner view controller
+        addChild(spinnerView)
+        spinnerView.view.frame = view.frame
+        view.addSubview(spinnerView.view)
+        spinnerView.didMove(toParent: self)
+
+
+    }
+    
     private func refreshView() {
-        textLabel.text = "City: \(weatherData?.location.name ?? ""), Temperature, \(weatherData?.current.temperature ?? 0), \nSunrise: \(weatherData?.current.astro.sunrise ?? "")"
+        
+        // wait two seconds to simulate some work happening
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                // then remove the spinner view controller
+                self.spinnerView.willMove(toParent: nil)
+                self.spinnerView.view.removeFromSuperview()
+                self.spinnerView.removeFromParent()
+                
+                self.textLabel.text = "City: \(self.weatherData?.location.name ?? ""), Temperature: \(self.weatherData?.current.temperature ?? 0), \nSunrise: \(self.weatherData?.current.astro.sunrise ?? "")"
+            }
+        
+        
     }
 
 
 }
 
+class SpinnerViewController: UIViewController {
+    var spinner = UIActivityIndicatorView(style: .large)
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .systemBackground
+
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        spinner.color = .gray
+        view.addSubview(spinner)
+
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
